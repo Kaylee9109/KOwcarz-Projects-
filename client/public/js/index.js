e6404a0dc031c52f1eb83b58af9b3b6bf7c1bdad
Kaylee9109 <kayleeowcarz60@gmail.com>
2025-07-10 14:27:42 -0400

    project 1 fully completed

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,13 +1,28 @@
-window.addEventListener("load", () => {
-  window.addEventListener("keypress", checkKeyPress)
-})
+
 
 
 /*
  * Check ascii code for spacebar and call goToLocation in global to change URL
  */
 function checkKeyPress(e) {
-  if (e.keyCode == 32) {
+  if(e.keyCode == 32) {
     goToLocation('/feed');
   }
 }
+function checkKeyPress(event) { 
+  if(event.code === 'Space') {
+    goToLocation('/feed');
+  }
+}
+
+document.addEventListener("keydown" , checkKeyPress);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeText = document.getElementById("fade_text");
+
+  if(fadeText){
+    fadeText.addEventListener("click" , function () {
+      goToLocation("/feed");
+    });
+  }
+});
